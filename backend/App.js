11ceec7c212aa81d9fbe256cc5bb3da776fdf566
gaga375 <baghelgagan787@gmail.com>
2025-06-24 23:41:30 +0530

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -1,4 +1,9 @@
 // import 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import path from 'path';
+
 import {connectoSockit} from './Controler/SocktManager.js';
 import express  from 'express';
 import {createServer} from "node:http"
@@ -33,8 +38,15 @@ app.use(cors());
 app.use(express.json({ limit: '40kb', extended: true }));
 app.use(express.urlencoded({limit:"40kb", extended:true}));
 app.use('/user',userControle)
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 // routs
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+
 app.get('/home', async (req ,res)=>{
     res.send("i love tamanna")
 })
